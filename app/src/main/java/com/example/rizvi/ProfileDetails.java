ee522bd9dd1e46a1ae62a7f32ebf8675cfe221b8
Dastageer Yakub Sayed <dy.sayed18@gmail.com>
2020-04-07 21:30:29 +0530

    Profile Added

--- app/src/main/java/com/example/rizvi/ProfileDetails.java
+++ app/src/main/java/com/example/rizvi/ProfileDetails.java
@@ -1,14 +1,54 @@
 package com.example.rizvi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileDetails extends AppCompatActivity {
+
+    TextView FullName, Email,PhoneNo;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_details);
+        PhoneNo = findViewById(R.id.Profile_Number);
+        FullName = findViewById(R.id.Profile_name);
+        Email = findViewById(R.id.Profile_email);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        final DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                PhoneNo.setText(documentSnapshot.getString("phone"));
+                FullName.setText(documentSnapshot.getString("fname"));
+                Email.setText(documentSnapshot.getString("email"));
+            }
+        });
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplication(),MainActivity.class));
+        finish();
     }
 }
